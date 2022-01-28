2d5d03a34feb918e48be16d91cae696d480bf09d
Annie-Ruth <annie.ruth169@gmail.com>
2022-01-28 14:58:10 +0000

    first commit for jenkins project

---
+++ src/main/java/JenkinsProject/jenkinsp/JenkinspApplication.java
@@ -0,0 +1,13 @@
+package JenkinsProject.jenkinsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JenkinsProject/jenkinsp/JenkinspApplicationTests.java
@@ -0,0 +1,13 @@
+package JenkinsProject.jenkinsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
